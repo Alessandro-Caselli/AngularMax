591f15bfa6f5cf1fc371b6e9631e1b7fa63e8724
Alex Caselli <case94aksv@gmail.com>
2021-07-21 11:41:38 +0200

    dddd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    TestComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
